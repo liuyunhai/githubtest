b836cea5fc24bc752631702c02809542c0f67629
Liuyunhai <1423415703@qq.com>
2022-09-05 21:16:08 +0800

    控制类提交

---
+++ src/main/java/com/github/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.github.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LYH
+ * @date 2022/9/5 - 21:13
+ */
+@RestController
+@RequestMapping("/github")
+public class UserController {
+}
